1bcecc88e9d5c1dc8f6aae5ef2c05629aa9abb2b
Test Automation Framework <automation@testautomation.org>
2026-05-06 23:18:47 +0300

    disable DB for now

--- test-automation-commons/src/main/java/org/testautomation/commons/config/TestConfig.java
+++ test-automation-commons/src/main/java/org/testautomation/commons/config/TestConfig.java
@@ -3,6 +3,9 @@ package org.testautomation.commons.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,7 +17,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * PlaywrightInstanceProvider is on the classpath
  */
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 @EntityScan("org.testautomation.commons.entities")
 @EnableJpaRepositories("org.testautomation.commons.repository")
 @ComponentScan(basePackages = {
